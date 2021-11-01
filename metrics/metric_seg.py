4bde15a4854f4e51020670bf8e236dd93249ecca
dahyedahye <dahyedarling@gmail.com>
2021-11-01 21:28:23 +0900

    minor fix

--- metrics/metric_seg.py
+++ metrics/metric_seg.py
@@ -3,7 +3,7 @@ import torch
 
 def evaluate_iou(pred, label):
   """ 
-  The prediction and gt are binary images. Prediction and gt have values 1 or 0  
+  The prediction and groundtruth are binary images. Input prediction and gt have values 1 or 0 in PyTorch tensor type.
   """
   flat_pred = torch.flatten(pred).cpu().detach().numpy()
   flat_label = torch.flatten(label).cpu().detach().numpy()
